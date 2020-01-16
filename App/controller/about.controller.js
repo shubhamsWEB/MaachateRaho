592994b80f2ebd165c72dde7bbd7aa0f4cc5e7d1
Shubham Agrawal <32464238+shubhamsWEB@users.noreply.github.com>
2020-01-16 18:08:16 +0530

    aboutus page

--- App/controller/about.controller.js
+++ App/controller/about.controller.js
@@ -1,7 +1,13 @@
 ﻿(function () {
     app.controller('aboutController', aboutC);
-    function aboutC() {
+    function aboutC($scope) {
         var vm = this;
+        vm.loading = true;
         
+        /*LOADER*/
+        $scope.$on('$viewContentLoaded', function(event)
+        { 
+         vm.loading = false;
+         });
     };
 })();
