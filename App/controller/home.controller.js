9e5c1e76d3a48d0b85f6a2369ab81f82b2470b61
Shubham <shubhama664@gmail.com>
2020-01-21 23:03:59 -0800

    landing page and events page

--- App/controller/home.controller.js
+++ App/controller/home.controller.js
@@ -2,6 +2,7 @@
     app.controller('homeController', homeC);
     function homeC($scope) {
         var vm = this;
+        vm.scroll = 0;
         vm.loading = true;
         
         /*LOADER*/
@@ -10,5 +11,26 @@
          vm.loading = false;
          });
          
+        $(function () {
+            $(window).keydown(function (e) {
+                var keyCode = window.event ? e.keyCode : e.which;
+                //UP
+                if (keyCode == 38) {
+                    if(vm.scroll !=0) {
+                    vm.scroll -= 630;
+                    } else {
+                        vm.scroll = 0;
+                    }
+                    $('html, body').animate({ scrollTop: vm.scroll }, 100);
+                    return false;
+                }
+                //DOWN
+                if (keyCode == 40) {
+                    vm.scroll += 630;
+                    $('html, body').animate({ scrollTop: vm.scroll }, 100);
+                    return false;
+                }
+            });
+        });
     };
 })();
