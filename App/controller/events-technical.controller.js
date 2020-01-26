c2a4654180c264e024a7aa7a3efc7f5f69f969f2
Shubham <shubhama664@gmail.com>
2020-01-26 01:05:53 -0800

    sub-events

--- App/controller/events-technical.controller.js
+++ App/controller/events-technical.controller.js
@@ -2,6 +2,11 @@
     app.controller('technicalController', technicalC);
     function technicalC() {
         var vm = this;
-    
+        vm.show = function() {
+        angular.forEach(document.getElementsByClassName('evt'),function(element,key) {
+            element.setAttribute("aria-expanded",true);
+            console.log(element.getAttribute("aria-expanded"));
+        });
+    };
     };
 })();
